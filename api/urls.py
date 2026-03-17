04eb52f81f27b1a6e2d29ecf61d69a46763833f3
Eddy Martinez <eddy.martinez@est.ulsa.edu.nim>
2026-03-17 10:44:50 -0600

    Enrutador

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PacienteViewSet
+
+
+router = DefaultRouter()
+router.register(r'pacientes', PacienteViewSet, basename='paciente')
