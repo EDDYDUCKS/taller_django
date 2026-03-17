30cccf6872f30195ecf95e47aef5505fa108a920
Eddy Martinez <eddy.martinez@est.ulsa.edu.nim>
2026-03-17 10:45:58 -0600

    correcion de errores en url.py

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PacienteViewSet
@@ -5,3 +6,8 @@ from .views import PacienteViewSet
 
 router = DefaultRouter()
 router.register(r'pacientes', PacienteViewSet, basename='paciente')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
